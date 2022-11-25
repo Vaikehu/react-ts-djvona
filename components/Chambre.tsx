1bd19a878e9f986d37368d04d27800494310146b
Vaikehu <118215415+Vaikehu@users.noreply.github.com>
2022-11-25 11:03:58 -1000

    1 file modified

--- components/Chambre.tsx
+++ components/Chambre.tsx
@@ -16,7 +16,7 @@ export default function App() {
   const [data, setData] = React.useState([]);
   React.useEffect(() => {
     (async () => {
-      const response = await getDataFromFirebase('bungalow-carrousel');
+      const response = await getDataFromFirebase('chambre-carrousel');
 
       console.log(response);
       setData(response);
@@ -42,8 +42,19 @@ export default function App() {
           />
         </SwiperSlide>
       ))}
+      <SwiperSlide>
+        <img
+          src={
+            'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.photoway.com%2Fimages%2Ftour-du-monde%2FTDM96_1664.jpg&imgrefurl=https%3A%2F%2Fwww.photoway.com%2Ffr%2Fdest%2FTDM96_7_poly2_moor_01.html&tbnid=wyEYuTdd2Ro03M&vet=12ahUKEwiS_LmzmMr7AhU1HjQIHQ7VA3EQMygTegUIARDFAQ..i&docid=9PY0jO6kU4h8VM&w=960&h=640&q=image%20de%20plage%20moorea&hl=fr&client=tablet-android-samsung-nf-rev1&ved=2ahUKEwiS_LmzmMr7AhU1HjQIHQ7VA3EQMygTegUIARDFAQ'
+          }
+          style={{ height: '25rem', objectFit: 'cover' }}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className="title text-center pd-5 m-5" data-swiper-parallax="-300"> BUNGALOW 
+      </div>
+      </SwiperSlide>
     </Swiper>
+    
   );
 }
-
-
