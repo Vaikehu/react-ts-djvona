24514020365e4d00016831a853aaf78421d0b695
Vaikehu <118215415+Vaikehu@users.noreply.github.com>
2022-11-28 12:08:25 -1000

    1 file modified

--- pages/Service.tsx
+++ pages/Service.tsx
@@ -6,8 +6,16 @@ const Service = () => (
   <div>
     <Navbar/>
     <Evennement/>
+    <div className="text-center"><h1>Wedding</h1>
+    les tarifs varie suivant la demande.
+    <hr/>
+    </div>
     <Evennement/>
+    <div className="text-center"><h2>Annivaire</h2>les tarifs varie suivant la demande.
+    <hr/>
+    </div>
     <Evennement/>
+    <div className="text-center"><h3>Honey Moon</h3>les tarifs varie suivant la demande.</div>
   </div>
 );
 
