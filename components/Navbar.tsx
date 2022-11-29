ce046e830c29f06178bb91429920181a19eda8c2
Vaikehu <118215415+Vaikehu@users.noreply.github.com>
2022-11-28 14:05:28 -1000

    1 file modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,8 @@ const Navbar = () => (
   <div>
     <nav className="navbar navbar-expand-lg bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand text-white d-flex" href="#">
-          Fare 'Vai'
+        <a className="nav-link active text-white" aria-current="page" href="#">
+          Accueil
         </a>
         <button
           className="navbar-toggler"
@@ -21,9 +21,6 @@ const Navbar = () => (
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active text-white" aria-current="page" href="#">
-                Accueil
-              </a>
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle text-white"
@@ -36,39 +33,49 @@ const Navbar = () => (
                 </a>
                 <ul className="dropdown-menu">
                   <li className="nav-item">
-                    <a className="dropdown-item" className="btn btn-primary btn-lg" href="/bedroom">
+                    <a
+                      className="dropdown-item btn btn-primary btn-lg"
+                      href="/bedroom"
+                    >
                       Chambres
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="dropdown-item" className=" btn btn-primary btn-lg "href="/Restaurants">
+                    <a
+                      className="dropdown-item btn btn-primary btn-lg "
+                      href="/Restaurants"
+                    >
                       Restaurants
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="dropdown-item" className=" btn btn-primary btn-lg" href="/Activite">Activité</a>
+                    <a
+                      className="dropdown-item btn btn-primary btn-lg"
+                      href="/Activite"
+                    >
+                      Activité
+                    </a>
                   </li>
                   <li className="nav-item">
-                    <a className="dropdown-item"className=" btn btn-primary btn-lg" href="/Service">Service</a>
+                    <a
+                      className="dropdown-item btn btn-primary btn-lg"
+                      href="/Service"
+                    >
+                      Service
+                    </a>
                   </li>
                   <li className="nav-item">
-                    <a className="dropdown-item"className=" btn btn-primary btn-lg" href="/Location">Location</a>
+                    <a
+                      className="dropdown-item btn btn-primary btn-lg"
+                      href="/Location"
+                    >
+                      Location
+                    </a>
                   </li>
                 </ul>
               </li>
             </li>
           </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
